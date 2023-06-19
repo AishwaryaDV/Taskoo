0fae03728923fc7f956ec574969f60675ef17e3b
AishwaryaDV <millamatew@gmail.com>
2023-06-19 15:35:35 +0530

    kanban-board changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,35 +1,186 @@
 import React from 'react';
-import { Toaster } from 'react-hot-toast';
-import CreateTask from './CreateTask.js';
-import ListTask from './ListTask.js';
 import './projects.css';
-import Sidebar from './Sidebar.js'
-import {useState, useEffect} from 'react';
+import Sidebar from './Sidebar';
+import { useState } from 'react';
 
 const Projects = () => {
-    const [tasks, setTasks] = useState([]);
-    console.log("Tasks", tasks);
+  const [cards, setCards] = useState([
+    { id: 1, text: "Add task", section: 0 },
+    { id: 2, text: "Add task", section: 0 },
+    { id: 3, text: "Add task", section: 0 }
+  ]);
 
-    // useEffect(()=>{
-    //   setTasks(JSON.parse(localStorage.getItem("tasks")));
-    // }, []);
+  const handleDragStart = (event, card) => {
+    event.dataTransfer.setData("card", JSON.stringify(card));
+  };
 
-    useEffect(() => {
-      const storedTasks = JSON.parse(localStorage.getItem("tasks"));
-      setTasks(storedTasks || []);
-    }, []);
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event, section) => {
+    event.preventDefault();
+    const draggedCard = JSON.parse(event.dataTransfer.getData("card"));
+    const updatedCards = cards.map((card) => {
+      if (card.id === draggedCard.id) {
+        return { ...card, section };
+      }
+      return card;
+    });
+    setCards(updatedCards);
+  };
+
+  const handleCardTextChange = (event, cardId) => {
+    const updatedCards = cards.map((card) => {
+      if (card.id === cardId) {
+        return { ...card, text: event.target.value };
+      }
+      return card;
+    });
+    setCards(updatedCards);
+  };
+
+  const deleteCard = (cardId) => {
+    const updatedCards = cards.filter((card) => card.id !== cardId);
+    setCards(updatedCards);
+  };
+
+  const createNewCard = (section) => {
+    const newCardId = cards.length + 1;
+    const newCard = { id: newCardId, text: `Add task`, section };
+    setCards([...cards, newCard]);
+  };
+
+  const getCardCount = (section) => {
+    return cards.filter((card) => card.section === section).length;
+  };
 
   return (
-    <>
-    <Toaster/>
-    <div className="project-section">
-        <div className="project-sidebar-section"><Sidebar /></div>
-        <div className="kanban-board-section">
-          <CreateTask tasks={tasks} setTasks={setTasks} />
-          <ListTask tasks={tasks} setTasks={setTasks} />
+    <div className="projects-page">
+      <div className="projects-sidebar-section"><Sidebar /></div>
+      <div className="projects-section">
+
+
+      <div className="todo-container">
+      <div
+        className="section-box"
+        onDragOver={handleDragOver}
+        onDrop={(event) => handleDrop(event, 1)}
+      >
+        <h2 className="kanban-header">Todo
+        <div className="card-count">{getCardCount(1)}</div>
+        </h2>
+
+        <button className="add-card-btn" onClick={() => createNewCard(1)}>
+          +
+        </button>
+        <div className="cards-container">
+          {cards
+            .filter((card) => card.section === 1)
+            .map((card) => (
+              <div
+                key={card.id}
+                draggable
+                onDragStart={(event) => handleDragStart(event, card)}
+                className="card"
+              >
+                <input
+                  type="text"
+                  value={card.text}
+                  onChange={(event) => handleCardTextChange(event, card.id)}
+                  className="card-input"
+                />
+                <button
+                  className="delete-card-btn"
+                  onClick={() => deleteCard(card.id)}
+                >
+                  x
+                </button>
+              </div>
+            ))}
         </div>
+      </div>
+
+      <div
+        className="section-box"
+        onDragOver={handleDragOver}
+        onDrop={(event) => handleDrop(event, 2)}
+      >
+        <h2 className="kanban-header">In progress 
+          <div className="card-count">{getCardCount(2)}</div>
+        </h2>
+        <button className="add-card-btn" onClick={() => createNewCard(2)}>
+          +
+        </button>
+        <div className="cards-container">
+          {cards
+            .filter((card) => card.section === 2)
+            .map((card) => (
+              <div
+                key={card.id}
+                draggable
+                onDragStart={(event) => handleDragStart(event, card)}
+                className="card"
+              >
+                <input
+                  type="text"
+                  value={card.text}
+                  onChange={(event) => handleCardTextChange(event, card.id)}
+                  className="card-input"
+                />
+                <button
+                  className="delete-card-btn"
+                  onClick={() => deleteCard(card.id)}
+                >
+                  x
+                </button>
+              </div>
+            ))}
+        </div>
+      </div>
+
+      <div
+        className="section-box"
+        onDragOver={handleDragOver}
+        onDrop={(event) => handleDrop(event, 3)}
+      >
+        <h2 className="kanban-header">Completed
+          <div className="card-count">{getCardCount(3)}</div>
+        </h2>
+        <button className="add-card-btn" onClick={() => createNewCard(3)}>
+          +
+        </button>
+        <div className="cards-container">
+          {cards
+            .filter((card) => card.section === 3)
+            .map((card) => (
+              <div
+                key={card.id}
+                draggable
+                onDragStart={(event) => handleDragStart(event, card)}
+                className="card"
+              >
+                <input
+                  type="text"
+                  value={card.text}
+                  onChange={(event) => handleCardTextChange(event, card.id)}
+                  className="card-input"
+                />
+                <button
+                  className="delete-card-btn"
+                  onClick={() => deleteCard(card.id)}
+                >
+                  x
+                </button>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
-    </>
+
+
+      </div>
+    </div>
   )
 }
 
